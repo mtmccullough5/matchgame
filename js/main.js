775c70867906830f535dd056e1dce61947794e75
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-08-27 15:59:07 -0600

    Assign button names and parameters

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 // Jquery
 // General form $(selector).action
+var cardbtns = $('.card');
+var card = {
+    state: "inactive",
+    frontimg: "A",
+    backimg: "Card Back"
+}
+console.log(cardbtns)
+for(i=0; i<cardbtns.length; i++){
+    cardbtns[i].name = "Card" + i;
+    cardbtns[i].state = "Inactive";
+    cardbtns[i].backimg = "Card Back";
+    cardbtns[i].frontimg = "Image" + i; 
+    console.log(i);
+}
+console.log(cardbtns)
+    
+
+console.log(card)
+
 $(document).ready(function(){
     $('.card').on('click',function(){
-        console.log("You got here")
+        console.log(this.frontimg)
+
     });
     
   });
