fb504fb77c8f79798a1493586a8d5adc74ac5908
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-08-27 14:54:59 -0600

    Add JS Connect Buttons

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Jquery
+// General form $(selector).action
+$(document).ready(function(){
+    $('.card').on('click',function(){
+        console.log("You got here")
+    });
+    
+  });
+   
+ 
+  
+  console.log("ItWorks")
